3c23c6f10b781af782f00066d666c9c3fceb5157
ajaynanba <nanbaajay@gmail.com>
2023-08-21 14:15:25 +0530

    Done

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+
+//        RWOnly obj = new RWOnly();
+//        obj.name = "Ajay";
+
+//        java: name has private access in com.driver.RWOnly
+        RWOnly obj = new RWOnly();
+        obj.setName("Ajay");
+        System.out.println(obj.getName());
+    }
   
 }
